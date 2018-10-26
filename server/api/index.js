4739de45aa54210afa59195c4c4265ee40958224
Jennifer Chen <jenniferchen@Jennifers-iMac.home>
2018-10-26 18:52:19 -0400

    fix

--- server/api/index.js
+++ server/api/index.js
@@ -1,11 +1,8 @@
 const router = require('express').Router()
 
-const product = require('./product')
-const user = require('./user')
-const category = require('./category')
-
-
 router.use('/auth', require('./auth'));
-
+router.use('/products', require('./product'))
+router.use('/user', require('./user'))
+router.use('/category', require('./category'))
 
 module.exports = router
