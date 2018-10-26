e8a8aac73a6090492bdc2f56f9bf4e647c595fab
Jarret Rose <jarretrose@gmail.com>
2018-10-26 09:06:31 -0400

    further modularized Products and ProductDetails

--- client/components/App.js
+++ client/components/App.js
@@ -1,6 +1,9 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux';
+
 import { getProducts } from '../store'
+
+import Products from './Products'
 import ProductDetails from './ProductDetails'
 
 class App extends Component {
@@ -11,17 +14,9 @@ class App extends Component {
   }
 
   render() {
-
-    const { products } = this.props
-    if (!products) { return null }
-
-    return (
+    return(
       <div>
-        <hr />
-        {
-          products.map(product => <ProductDetails key={product.id} product = { product } />
-          )
-        }
+        <Products />
       </div>
     )
   }
