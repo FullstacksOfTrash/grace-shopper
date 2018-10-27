564d13373f89736a52d71a76c05e62480d6e126b
Jennifer Chen <jenniferchen@Jennifers-iMac.home>
2018-10-27 11:42:41 -0400

    asdf

--- server/api/index.js
+++ server/api/index.js
@@ -1,7 +1,7 @@
 const router = require('express').Router()
 
 router.use('/auth', require('./auth'));
-router.use('/products', require('./product'))
+router.use('/products', require('./products'))
 router.use('/user', require('./user'))
 router.use('/category', require('./category'))
 router.use('/reviews', require('./review'))
