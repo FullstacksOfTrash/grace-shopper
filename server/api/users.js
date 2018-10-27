d594656c91e7ce1de02f5af93f0d2d727590bbcc
Zijian Yan <zi@zijianyan.com>
2018-10-27 17:34:01 -0400

    nest orders under user routes

--- server/api/users.js
+++ server/api/users.js
@@ -2,33 +2,6 @@ const router = require('express').Router()
 
 const { User, Order, LineItem, Review } = require('../db')
 
-router.put('/:userId/orders/:orderId/lineitems/:id', (req, res, next) => {       //used to incrementing or decrementing lineItem
-    LineItem.findById(req.params.lineItemId)
-    .then(lineItem => lineItem.update(req.body))
-    .then(lineItem => res.send(lineItem))
-    .catch(next)
-})
-
-
-router.delete('/:userId/orders/:orderId/lineitems/:id', (req, res, next) => {   //when lineItem has been reduced to zero, a delete request will be called to destroy it 
-    LineItem.findOne({
-        where: {
-            id: req.params.id,
-            orderId: req.params.orderId
-        }   
-    })
-    .then(lineItem => lineItem.destroy())
-    .then(() => res.sendStatus(204))
-    .catch(next)
-})
-
-
-router.post('/:userId/orders/:orderId/lineitems', (req, res, next) => {         //route will be sent a productId in req.body to determine waht product a lineItem is related to
-    LineItem.create({ productId: req.body.productId, orderId: req.params.orderId})
-    .then(lineItem => res.send(lineItem))
-    .catch(next)
-})
-
 router.get('/:userId/orders', async(req, res, next) => {
     const temp = {
         userId : req.body.userId,
@@ -43,15 +16,47 @@ router.get('/:userId/orders', async(req, res, next) => {
             where: {
                 userId: req.params.userId
             },
-            include: [ { model:LineItem } ]
+            include: [ LineItem ]
         })
-
         res.send(orders)
     } catch(err){
         next(err)
     }
 })
 
+router.put('/:userId/orders/:orderId', (req, res, next)=> { //edit a user's order, for example, change status from 'CART' to 'ORDER'
+    Order.findById(req.params.id)
+        .then(order => order.update(req.body))
+        .then(order => res.send(order))
+        .catch(next)
+})
+
+router.put('/:userId/orders/:orderId/lineitems/:id', (req, res, next) => {       //used to incrementing or decrementing lineItem
+    LineItem.findById(req.params.lineItemId)
+        .then(lineItem => lineItem.update(req.body))
+        .then(lineItem => res.send(lineItem))
+        .catch(next)
+})
+
+router.delete('/:userId/orders/:orderId/lineitems/:id', (req, res, next) => {   //when lineItem has been reduced to zero, a delete request will be called to destroy it 
+    LineItem.findOne({
+        where: {
+            id: req.params.id,
+            orderId: req.params.orderId
+        }   
+    })
+    .then(lineItem => lineItem.destroy())
+    .then(() => res.sendStatus(204))
+    .catch(next)
+})
+
+router.post('/:userId/orders/:orderId/lineitems', (req, res, next) => {         //route will be sent a productId in req.body to determine waht product a lineItem is related to
+    LineItem.create({ productId: req.body.productId, orderId: req.params.orderId})
+    .then(lineItem => res.status(201).send(lineItem))
+    .catch(next)
+})
+
+// User's reviews
 router.get('/:userId/reviews', (req, res, next) => {
     Review.findAll({
         where: {
@@ -63,3 +68,5 @@ router.get('/:userId/reviews', (req, res, next) => {
 })
 
 module.exports = router
+
+
