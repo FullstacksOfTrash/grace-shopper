5bd4c21a8f7f0d383cc6ca5ed4ad3fe06b979fd3
Jarret Rose <jarretrose@gmail.com>
2018-10-28 10:31:46 -0400

    commented out review route schema from server/api/index.js and server/api/review.js

--- server/api/index.js
+++ server/api/index.js
@@ -4,7 +4,8 @@ router.use('/auth', require('./auth'));
 router.use('/products', require('./products'))
 router.use('/users', require('./users'))
 router.use('/categories', require('./categories'))
-router.use('/reviews', require('./review'))
 router.use('/orders', require('./orders'))
+
+// router.use('/reviews', require('./review'))
 
 module.exports = router
--- server/api/review.js
+++ server/api/review.js
@@ -1,10 +1,10 @@
-const router = require('express').Router()
-const { Review } = require('../db')
+// const router = require('express').Router()
+// const { Review } = require('../db')
 
-router.get('/', (req, res, next) => {
-  Review.findAll()
-    .then(reviews => res.send(reviews))
-    .catch(next)
-})
+// router.get('/', (req, res, next) => {
+//   Review.findAll()
+//     .then(reviews => res.send(reviews))
+//     .catch(next)
+// })
 
-module.exports = router;
+// module.exports = router;
