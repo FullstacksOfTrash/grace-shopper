86f41fa561abe6965d5635679e08f7b2f29f115e
Cang Truong <cangtruong1@gmail.com>
2018-10-28 11:27:30 -0400

    loaded orders of user in orderHistory page and added util func

--- client/components/OrderHistory.js
+++ client/components/OrderHistory.js
@@ -1,22 +1,43 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
+import { getProduct, lineItemsTotalQuant } from '../store/utils'
 
 class OrderHistory extends Component {
   render () {
-    const {orders} = this.props
+    const {orders, products} = this.props
     if(!orders) { return null }
 
     return (
       <div>
         <h3>Order History</h3>
+        <div>
+          {
+            orders.map(order => (
+              <ul>Order# {order.id}
+              <li>Total: ${lineItemsTotalQuant(order.lineItems,products)}</li>
+              <li>Order Placed: {order.createdAt}</li>
+              {
+                order.lineItems.map(item => (
+                <ul>
+                  <li>{getProduct(item.productId,products).name} - {item.quantity}</li>
+                </ul>
+                ))
+              }
+              </ul>
+            ))
+          }
+        </div>
       </div>
     )
   }
 }
 
-const mapStateToProps = ({orders}) => {
+const mapStateToProps = ({orders, products}) => {
+
+
   return {
-    orders: orders.filter(order => order.status === 'ORDER')
+    orders: orders.filter(order => order.status === 'ORDER'),
+    products
   }
 }
 
