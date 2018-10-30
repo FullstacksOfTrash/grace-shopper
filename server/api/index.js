6e8ebb879081d09cdc53e8e5729a6eb1abd01942
Jennifer Chen <jenniferchen@Jennifers-iMac.home>
2018-10-30 15:34:57 -0400

    integrated stripe

--- server/api/index.js
+++ server/api/index.js
@@ -6,5 +6,6 @@ router.use('/users', require('./users'))
 router.use('/categories', require('./categories'))
 router.use('/orders', require('./orders'))
 router.use('/reviews', require('./reviews'))
+router.use('/payment', require('./payment'))
 
 module.exports = router
---
+++ server/api/payment.js
@@ -0,0 +1,23 @@
+const { key2 } = require('../../apiKeys')
+const stripe = require('stripe')(key2)
+const router = require('express').Router()
+
+
+router.post('/charge', async (req, res, next) => {
+    console.log(req.body)
+    try {
+        const { status } = await stripe.charges.create({
+            amount: 2200,
+            currency: "usd",
+            description: 'an ex charge',
+            source:req.body
+        })
+        console.log(status)
+        res.send({status})
+    } catch(err){
+        console.log(err)
+        res.status(500).end()
+    }
+})
+
+module.exports = router
