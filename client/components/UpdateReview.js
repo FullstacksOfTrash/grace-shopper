f926fa5bbb6bff37c731fcdc184189662616516a
Jarret Rose <jarretrose@gmail.com>
2018-10-30 17:23:58 -0400

    removed console logs

--- client/components/UpdateReview.js
+++ client/components/UpdateReview.js
@@ -17,7 +17,6 @@ class UpdateReview extends React.Component{
 
     handleChange (event) {
         this.setState({[event.target.name]: event.target.value})
-        console.log(this.state)
       }
 
     handleSubmit(event){
