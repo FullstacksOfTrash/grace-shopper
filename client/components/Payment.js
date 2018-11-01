dcc5659fdfc4aa7687bcbd3ab9f6a19964917416
Jarret Rose <jarretrose@gmail.com>
2018-10-31 23:16:39 -0400

    took a minute to reset all the spacing on all of the files to 2 spaces / tab = 2 spaces

--- client/components/Payment.js
+++ client/components/Payment.js
@@ -2,34 +2,34 @@ import React from 'react'
 import axios from 'axios'
 import { injectStripe, CardElement } from 'react-stripe-elements'
 
-class PaymentForm extends React.Component{
-    constructor(){
-        super()
-        this.state = { complete: false, error: '' }
-        this.submit = this.submit.bind(this)
-    }
-    async submit(event){
-        event.preventDefault()
-        const { stripe , order } = this.props //assuming order will be passed down here
-        const { token } = await stripe.createToken({name: 'harry'})       //will turn into thunk later //pass in customer name and id for the name  prop
-        console.log(token)
-        const response = await axios.post('/api/payment/charge', token.id, { headers: {"Content-Type": 'text/plain' }})         //will change route will be passed token id as well as the order total to complete charge
-        if(response.data.status === 'succeeded'){
-            this.setState({complete: true})
-        }
-    }
-    render(){ 
-        if(this.state.complete){
-            return <h1>Purchase has been completed</h1>
-        }
-        return (
-            <div>
-                <p>Would you like to complete your purchase?</p>
-                <CardElement/>
-                <button onClick={this.submit}>Send</button>
-            </div>
-        )
-    }
+class PaymentForm extends React.Component {
+	constructor() {
+		super()
+		this.state = { complete: false, error: '' }
+		this.submit = this.submit.bind(this)
+	}
+	async submit(event) {
+		event.preventDefault()
+		const { stripe, order } = this.props //assuming order will be passed down here
+		const { token } = await stripe.createToken({ name: 'harry' })       //will turn into thunk later //pass in customer name and id for the name  prop
+		console.log(token)
+		const response = await axios.post('/api/payment/charge', token.id, { headers: { "Content-Type": 'text/plain' } })         //will change route will be passed token id as well as the order total to complete charge
+		if (response.data.status === 'succeeded') {
+			this.setState({ complete: true })
+		}
+	}
+	render() {
+		if (this.state.complete) {
+			return <h1>Purchase has been completed</h1>
+		}
+		return (
+			<div>
+				<p>Would you like to complete your purchase?</p>
+				<CardElement />
+				<button onClick={this.submit}>Send</button>
+			</div>
+		)
+	}
 
 }
 
--- server/api/payment.js
+++ server/api/payment.js
@@ -4,20 +4,20 @@ const router = require('express').Router()
 
 
 router.post('/charge', async (req, res, next) => {
-    console.log(req.body)
-    try {
-        const { status } = await stripe.charges.create({
-            amount: 2200,
-            currency: "usd",
-            description: 'an ex charge',
-            source:req.body
-        })
-        console.log(status)
-        res.send({status})
-    } catch(err){
-        console.log(err)
-        res.status(500).end()
-    }
+	console.log(req.body)
+	try {
+		const { status } = await stripe.charges.create({
+			amount: 2200,
+			currency: "usd",
+			description: 'an ex charge',
+			source: req.body
+		})
+		console.log(status)
+		res.send({ status })
+	} catch (err) {
+		console.log(err)
+		res.status(500).end()
+	}
 })
 
 module.exports = router
