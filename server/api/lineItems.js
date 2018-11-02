2300af9d6912cea82572764ac95de57fc2e2501b
Cang Truong <cangtruong1@gmail.com>
2018-11-01 21:26:46 -0400

    added lineItems to localStorage

---
+++ server/api/lineItems.js
@@ -0,0 +1,18 @@
+const router = require('express').Router()
+module.exports = router
+const LineItem = require('../db/models/LineItem')
+
+router.post('/', (req, res, next) => {
+  //route will be sent a productId in req.body to determine waht product a lineItem is related to
+	LineItem.create({ productId: req.body.productId})
+		.then(lineItem => res.status(201).send(lineItem))
+		.catch(next)
+})
+
+router.put('/:id', (req, res, next) => {
+  //used to incrementing or decrementing lineItem
+	LineItem.findById(req.params.id)
+		.then(lineItem => lineItem.update(req.body))
+		.then(lineItem => res.send(lineItem))
+		.catch(next)
+})
