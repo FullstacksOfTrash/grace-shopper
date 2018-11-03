59392d1caac16b47c8424d27de00547a121f665e
Cang Truong <cangtruong1@gmail.com>
2018-11-03 16:15:51 -0400

    separated guest and logged in user lineItem functionality

--- client/store/utils.js
+++ client/store/utils.js
@@ -34,3 +34,36 @@ export const findLocalLineItem = (productId) => {
   const localCart = JSON.parse(window.localStorage.getItem('lineItems'))
   return localCart.lineItems.find(item => item.productId === productId*1)
 }
+
+export const guestIncrementLineItem = (product) => {
+  const localCart = getLocalCart()
+  const item = findLocalLineItem(product.id)
+  let cart = {}
+  if(item && localCart){ // if we already created a lineItem for this product, increase the quant of that lineItem
+    const filtered = localCart.lineItems.filter(item => item.productId !== product.id*1 )
+    const updatedLineItem = {...item, quantity: item.quantity+1}
+    cart = {...cart, lineItems: [...filtered, updatedLineItem]}
+  }
+  if(!item && localCart) { // if we're adding a new product to the cart
+    cart = {...localCart, lineItems: [...localCart.lineItems, {quantity: 1, productId: product.id}]}
+  }
+  // set the new object on localStorage
+  window.localStorage.removeItem('lineItems')
+  window.localStorage.setItem('lineItems', JSON.stringify(cart))
+}
+
+export const guestDecrementLineItem = (product) => {
+  const localCart = getLocalCart()
+  const item = findLocalLineItem(product.id)
+  if(item.quantity === 1 && localCart){ // deleting the line item
+    const filtered = localCart.lineItems.filter(item => item.productId !== product.id*1 )
+    cart = {...cart, lineItems: filtered}
+  } else { // decrementing line item quantity
+    const filtered = localCart.lineItems.filter(item => item.productId !== product.id*1 )
+    const updatedLineItem = {...item, quantity: item.quantity-1}
+    cart = {...cart, lineItems: [...filtered, updatedLineItem]}
+  }
+  // set the new object on localStorage
+  window.localStorage.removeItem('lineItems')
+  window.localStorage.setItem('lineItems', JSON.stringify(cart))
+}
