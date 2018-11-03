a6400959a92c8f125d49330ad5dceecfeb0dd39e
Jarret Rose <jarretrose@gmail.com>
2018-11-03 01:02:09 -0400

    added uuid to models... but not implemented

--- server/db/models/Order.js
+++ server/db/models/Order.js
@@ -1,6 +1,11 @@
 const conn = require('../conn');
 
 const Order = conn.define('order', {
+  // id: {
+  //   type: conn.Sequelize.UUID,
+  //   defaultValue: conn.Sequelize.UUIDV4,
+  //   primaryKey: true
+  // },
   status: {
     type: conn.Sequelize.ENUM('CART', 'ORDER'),
   },
--- server/db/models/User.js
+++ server/db/models/User.js
@@ -1,6 +1,11 @@
 const conn = require('../conn');
 
 const User = conn.define('user', {
+  // id: {
+  //   type: conn.Sequelize.UUID,
+  //   defaultValue: conn.Sequelize.UUIDV4,
+  //   primaryKey: true
+  // },
   firstName: {
     type: conn.Sequelize.STRING,
     allowNull: false,
