37622c32fec3d838af16fa085a550776daa8c666
Jarret Rose <jarretrose@gmail.com>
2018-11-04 11:47:01 -0500

    guh push

--- client/components/LogIn.js
+++ client/components/LogIn.js
@@ -3,6 +3,30 @@ import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
 import { logIn } from '../store/thunks';
 
+import Button from '@material-ui/core/Button';
+import TextField from '@material-ui/core/TextField';
+import PropTypes from 'prop-types';
+import classNames from 'classnames';
+import { withStyles } from '@material-ui/core/styles';
+import MenuItem from '@material-ui/core/MenuItem';
+
+const styles = theme => ({
+  container: {
+    display: 'flex',
+    flexWrap: 'wrap',
+  },
+  textField: {
+    marginLeft: theme.spacing.unit,
+    marginRight: theme.spacing.unit,
+  },
+  dense: {
+    marginTop: 16,
+  },
+  menu: {
+    width: 200,
+  },
+});
+
 class LogIn extends React.Component {
   constructor() {
     super();
@@ -26,24 +50,45 @@ class LogIn extends React.Component {
   render() {
     const { email, password } = this.state;
     const { handleChange, handleSubmit } = this;
+    const { classes } = this.props;
     return (
       <div>
         <form onSubmit={handleSubmit}>
-          <label htmlFor="email">Email: </label>
-          <input
+          {/* <label htmlFor="email">Email: </label> */}
+          <TextField
+          id="outlined-email-input"
+          label="Email"
+          className={classes.textField}
+          type="email"
+          name="email"
+          autoComplete="email"
+          margin="normal"
+          variant="outlined"
+        />
+          {/* <input
             type="email"
             name="email"
             value={email}
             onChange={handleChange}
-          />
-          <label htmlFor="password">Password: </label>
-          <input
+          /> */}
+          {/* <label htmlFor="password">Password: </label> */}
+          <TextField
+          id="outlined-password-input"
+          label="Password"
+          className={classes.textField}
+          type="password"
+          autoComplete="current-password"
+          margin="normal"
+          variant="outlined"
+        />
+          {/* <input
             type="password"
             name="password"
             value={password}
             onChange={handleChange}
-          />
-          <button type="submit">Log in</button>
+          /> */}
+          {' '}
+          <Button variant="contained" color="primary" type="submit" >Log in</Button>
         </form>
         <Link to="/signup">Sign Up!</Link>
       </div>
@@ -51,13 +96,14 @@ class LogIn extends React.Component {
   }
 }
 
+LogIn.propTypes = {
+  classes: PropTypes.object.isRequired,
+};
+
 const mapDispatchToProps = (dispatch, ownProps) => {
   return {
     loggingIn: (credentials, history) => dispatch(logIn(credentials, history)),
   };
 };
 
-export default connect(
-  null,
-  mapDispatchToProps
-)(LogIn);
+export default connect(null,mapDispatchToProps)(withStyles(styles)(LogIn));
