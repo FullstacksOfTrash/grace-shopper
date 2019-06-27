7f7c3bbfb582b1b4e4d9ab9b76e9b80ba905e503
Jarret Rose <jarretrose@gmail.com>
2019-06-27 09:44:14 -0400

    typo

--- server/db/index.js
+++ server/db/index.js
@@ -112,7 +112,7 @@ const syncAndSeed = async () => {
     }),
     Product.create({
       name: "Mid-Sized Giant's Chopsticks",
-      imageUrl: '/images//crutches_small.jpg',
+      imageUrl: '/images/crutches_small.jpg',
       price: 25,
       stock: 300,
       description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
